b6343d75c5b340523a27c8c5a20469bd00530abb
arif matubber <am@hoxro.com>
2024-10-29 19:41:35 +0600

    s

---
+++ random_images/script.js
@@ -0,0 +1,15 @@
+const imageContainer = document.querySelector(".container")
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", () => {
+    // console.log("clicked")
+    imageNumber = 10
+    addNewImages()
+})
+
+function addNewImages() {
+    for (let index = 0; index < imageNumber; index++) {
+        const newImg = document.createElement("img")
+        newImg.src = `https://picsum.photos/300?random=${Math.floor(Math.random() * 200)}`
+        imageContainer.appendChild(newImg)
+    }
+}
